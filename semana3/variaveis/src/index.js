2acad88c57c12b249082ab1b486fd70452353e06
Mateus Duarte <mateusduarte@mateuss-mbp.home>
2020-09-28 15:21:56 -0300

    exercicios variaveis

---
+++ semana3/variaveis/src/index.js
@@ -0,0 +1,49 @@
+// * 1 - primeiro será impresso (10) e depois (10, 5)
+
+// * 2 - (10, 10, 10)
+
+// let nome;
+// let idade;
+// console.log(typeof nome);
+// console.log(typeof idade);
+// idade = prompt("Qual a sua idade?")
+// nome = prompt("Qual o seu nome?")
+// console.log(typeof nome);
+// console.log(typeof idade);
+// console.log(`Ola ${nome}, você tem ${idade} anos`)
+
+// * Undefined foi impresso pois as variaveis não tem valor atribuido.
+
+// let pergunta1 = prompt('Qual sua comida favorita?')
+// console.log(`Resposta: ${pergunta1}`)
+// let pergunta2 = prompt('Qual o nome da sua mãe?')
+// console.log(`Resposta: ${pergunta2}`)
+// let pergunta3 = prompt('Qual o nome do seu pai?')
+// console.log(`Resposta: ${pergunta3}`)
+// let pergunta4 = prompt('Qual sua cor favorita?')
+// console.log(`Resposta: ${pergunta4}`)
+// let pergunta5 = prompt('Qual seu signo?')
+// console.log(`Resposta: ${pergunta5}`)
+
+// let array = ['palmito', 'beterraba', 'brocoli', 'batata', 'chocolate']
+// console.log(array)
+// console.log(`Essas são minhas comidas preferidas:`)
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+// console.log(array[3])
+// console.log(array[4])
+
+// let pergunta1 = prompt('Qual sua comida favorita?')
+
+// array[1] = pergunta1
+// console.log(array)
+
+let pergunta1 = 'Você escovou o dente hoje?'
+let pergunta2 = 'Você sabe dançar?'
+let pergunta3 = 'Gosta de queijo?'
+array = [pergunta1, pergunta2, pergunta3]
+array2 = [true, false, true]
+console.log(array[0]+array2[0])
+console.log(array[1]+array2[1])
+console.log(array[2]+array2[2])
