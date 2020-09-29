e749a44440d3a037bbfebe35dfec66d7268ab78e
Mateus Duarte <mateusduarte@mateuss-mbp.home>
2020-09-29 14:57:32 -0300

    exercicio com desafio

---
+++ semana3/operadores/index.js
@@ -0,0 +1,90 @@
+
+// 1
+// a.  false
+// b.  false
+// c.  true
+// e.  boolean
+
+
+// 2.
+// a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. 0,19
+// f. 19
+
+
+
+// 1
+
+// let idade = prompt('Qual sua idade?')
+
+// let idadeMelhorAmigo = prompt('Qual idade do seu melhor amigo?')
+
+// idade = Number(idade)
+// idadeMelhorAmigo = Number(idadeMelhorAmigo)
+
+// const diferenca = idade - idadeMelhorAmigo
+// if(diferenca>0){
+//     console.log("Sua idade é maior do que a do seu melhor amigo? true")
+// } else console.log ("Sua idade é maior do que a do seu melhor amigo? false")
+
+// console.log(diferenca)
+
+
+// 2
+// let numeroPar = prompt('digite um número par')
+// numeroPar = Number(numeroPar)
+// console.log(numeroPar%2)
+
+// número par sempre tem resto 0 e número impar sempre tem resto 1
+
+
+// 3
+// listaDeTarefas = []
+// let tarefa1 = prompt('digite uma tarefa que precisa fazer hoje')
+// let tarefa2 = prompt('digite uma tarefa que precisa fazer hoje')
+// let tarefa3 = prompt('digite uma tarefa que precisa fazer hoje')
+// listaDeTarefas.push(tarefa1)
+// listaDeTarefas.push(tarefa2)
+// listaDeTarefas.push(tarefa3)
+// console.log(listaDeTarefas)
+// let indice = prompt('digite o indice de uma tarefa realizada')
+// indice = Number(indice)
+// listaDeTarefas.splice(indice,1)
+// console.log(listaDeTarefas)
+
+
+// let nome = prompt('digite seu nome')
+// let email = prompt('digite seu email')
+// console.log("O email " + email + " foi cadastrado com sucesso. Seja bem-vinda(o), "+ nome+"!")
+
+// let Fah = 77
+// let Kel = (Fah - 32)*5/9 + 273.15
+// console.log("77°F é "+Kel+"K")
+// let Cel = 80
+// Fah = (Cel)*9/5 + 32
+// console.log("80°C é "+Fah+"°F")
+// Cel = 30
+// Fah = (Cel)*9/5 + 32
+// Kel=Cel+273
+// console.log("30°C é "+Fah+"°F"+"ou "+Kel+"K")
+// Cel1 = prompt('digite uma temp em celsius')
+// Cel1 = Number(Cel1)
+// Fah = (Cel1)*9/5 + 32
+// Kel=Cel1+273
+// console.log("30°C é "+Fah+"°F"+"ou "+Kel+"K")
+
+
+// let quilowatts = prompt('quilowatts consumidos')
+// quilowatts = Number(quilowatts)
+// let valor = quilowatts * .05
+// console.log("Valor a ser pago pré desconto: R$"+valor)
+// let desconto = prompt('porcentagem de desconto?')
+// desconto = Number(desconto)
+// desconto = desconto/100
+// valor2 = valor - (valor*desconto)
+// console.log("Valor a ser pago pós desconto: R$"+valor2)
+
+
